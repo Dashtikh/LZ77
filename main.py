1f691028f5e288b39e8bd72200998aaec869a1ff
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2021-09-29 10:45:17 +0330

    module added

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-code = input("please insert a coded string: ")
-code = code.replace("<", "")
-code = code.replace(">", "")
-code = code.replace(",", "")
-decode = ""
-counter = 0
-start = 0
-while counter < len(code):
-    if code[counter] == "0":
-        decode = decode + code[counter + 2]
-        counter = counter + 3
-    else:
-        for letter in range(0, int(code[counter + 1])):
-            start = (-1) * int(code[counter])
-            decode = decode + decode[start]
-        decode = decode + code[counter + 2]
-        counter = counter + 3
-print(decode)
+# module for lz77 decoder and encoder
+
+# decoder function
+def decoder(code):
+    code = code.replace("<", "")
+    code = code.replace(">", "")
+    code = code.replace(",", "")
+    decode = ""
+    counter = 0
+
+    start = 0
+
+    while counter < len(code):
+        if code[counter] == "0":
+            decode = decode + code[counter + 2]
+            counter = counter + 3
+        else:
+            for letter in range(0, int(code[counter + 1])):
+                start = (-1) * int(code[counter])
+                decode = decode + decode[start]
+            decode = decode + code[counter + 2]
+            counter = counter + 3
+
+    return decode
