520742bb92fc31953a34a899de590a0993ff7384
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2021-09-29 12:45:28 +0330

    lz78 decoder completed

--- lz78.py
+++ lz78.py
@@ -1,1 +1,22 @@
 # module for lz78 decoder and encoder
+# decoder function
+def decoder78(code):
+    code = code.replace("<", "")
+    code = code.replace(">", "")
+    code = code.replace(",", "")
+    decode = ""
+    counter = 0
+    decode = [""]
+    decodestring = ""
+    start = 0
+
+    while counter < len(code):
+        if code[counter] == "0":
+            decode.append(code[counter + 1])
+            counter = counter + 2
+        else:
+            decode.append(decode[int(code[counter])] + code[counter + 1])
+            counter = counter + 2
+    for letter in decode:
+        decodestring = decodestring + letter
+    return decodestring
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from lz77 import *
+from lz78 import *
 
 code = input("please insert a coded string: ")
 
-decode = decoder(code)
+decode = decoder78(code)
 
 print(decode)
